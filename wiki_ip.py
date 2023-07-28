f84e4aa2b2d61fee22f6af194825a2f52fb67395
mbakhshali <135727560+mbakhshali@users.noreply.github.com>
2023-07-28 13:14:29 +0330

    Update wiki_ip.py
    adds loop through pages

--- wiki_ip.py
+++ wiki_ip.py
@@ -5,27 +5,43 @@ import re
 
 dicIP = {}
 ls = []
-def ip2location(title):
-    html = urlopen('https://en.wikipedia.org/w/index.php?title='+title+'&action=history')
+
+
+url = 'https://en.wikipedia.org/w/index.php?title=Ministry_of_Communications_(India)&action=history'
+
+while True:
+    def ip2location(url):
+        html = urlopen(url)
+        bsObj = BeautifulSoup(html, features="html.parser")
+        for links in bsObj.findAll('a', {'class' : 'mw-userlink mw-anonuserlink'}):
+            if links is not None:
+                if links not in dicIP:
+                    ip = finder(links.get_text())
+                    dicIP[links.get_text()] = ip
+        if len(dicIP) <=0:
+            return 'Nothing found!'
+
+
+    def finder(ip):
+        locationFile = urlopen('http://ip-api.com/json/' + ip).read().decode('utf-8')
+        result = json.loads(locationFile)
+        country = result.get('country')
+        city = result.get('city')
+        dic = {}
+        dic[country] = city
+        return dic
+
+    ip2location(url)
+    html = urlopen(url)
     bsObj = BeautifulSoup(html, features="html.parser")
-    for links in bsObj.findAll('a', {'class' : 'mw-userlink mw-anonuserlink'}):
-        if links is not None:
-            if links not in dicIP:
-                ip = finder(links.get_text())
-                dicIP[links.get_text()] = ip
-    if len(dicIP) <=0:
-        return 'Nothing found!'
-    return dicIP
 
+    nextPage = bsObj.find('a', {'class' : 'mw-nextlink'})
 
-def finder(ip):
-    locationFile = urlopen('http://ip-api.com/json/' + ip).read().decode('utf-8')
-    result = json.loads(locationFile)
-    country = result.get('country')
-    city = result.get('city')
-    dic = {}
-    dic[country] = city
-    return dic
+    if nextPage is not None:
+        if len(nextPage) == 1:
+            url = 'https://en.wikipedia.org'+nextPage.attrs['href']
+            ip2location(url)
+    else:
+        break
 
-
-print(ip2location('Minato,_Tokyo'))
+print(dicIP)
